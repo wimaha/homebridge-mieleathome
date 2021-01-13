daa6500b6312541236260e8e237315ecf27ac578
QuickSander <sander@vanwoensel.me>
2021-01-13 09:55:16 +0100

    refactor: Remove dependency on request package for platform.

--- src/platform.ts
+++ src/platform.ts
@@ -1,13 +1,17 @@
+// Apacche License
+// Copyright (c) 2020, Sander van Woensel
+
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import { MieleHoodPlatformAccessory } from './mieleHoodPlatformAccessory';
 import { MieleWasherDryerPlatformAccessory } from './mieleWasherDryerPlatformAccessory';
 
-import request from 'request';
+import axios from 'axios';
 
 //-------------------------------------------------------------------------------------------------
-// Main class
+// Class MieleAtHomePlatform
+//-------------------------------------------------------------------------------------------------
 export class MieleAtHomePlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
@@ -19,7 +23,7 @@ export class MieleAtHomePlatform implements DynamicPlatformPlugin {
   public readonly pollInterval: number = parseInt(<string>this.config.pollInterval);
   public readonly baseURL = 'https://api.mcs3.miele.com/v1/devices';
 
-  
+  // Readonly constants
   public readonly WASHER_ID = 1;
   public readonly DISH_WASHER_ID = 7;
   public readonly HOOD_RAW_ID = 18;
@@ -61,25 +65,27 @@ export class MieleAtHomePlatform implements DynamicPlatformPlugin {
 
   //-----------------------------------------------------------------------------------------------
   // Discover devices
-  private discoverDevices() {
+  private async discoverDevices() {
 
+    // const config = {
+    //   'method': 'GET',
+    //   'url': this.baseURL,
+    //   'headers': { 
+    //     'Authorization': this.token,
+    //     'Content-Type': 'application/json',
+    //   },
+    // };
     const config = {
-      'method': 'GET',
-      'url': this.baseURL,
       'headers': { 
         'Authorization': this.token,
         'Content-Type': 'application/json',
       },
     };
 
-    request(config, (err, res, body) => {
-      if (err) {
-        return(this.log.debug(err));
-      }
-      const response = JSON.parse(body);
-      this.log.debug('Platform::discoverDevices: ', response);
-
-      const allDevices = Object.keys(response).map(key => response[key]);
+    try {
+      const response = await axios.get(this.baseURL, config);
+      this.log.debug('Discovered devices: ', response.data);
+      const allDevices = Object.keys(response.data).map(key => response.data[key]);
 
       // Loop over the discovered devices and register each one if it has not already been registered
       for (const device of allDevices) {
@@ -93,30 +99,6 @@ export class MieleAtHomePlatform implements DynamicPlatformPlugin {
         this.log.info(`Discovered device: id: ${deviceObject.uniqueId}, `+
                       `name: ${deviceObject.displayName}, model: ${deviceObject.modelNumber}`);
 
-        // Determine device type
-        let platformAccessoryType;
-
-        switch (device.ident.type.value_raw) {
-          case this.HOOD_RAW_ID: {
-            platformAccessoryType = MieleHoodPlatformAccessory;
-            break;
-          }
-
-          case this.WASHER_DRYER_ID:
-          case this.WASHER_ID: {
-            platformAccessoryType = MieleWasherDryerPlatformAccessory;
-            break;
-          }
-          
-          default: {
-            this.log.info(`Skipping unsupported device "${deviceObject.displayName}" `+
-                          `with raw type value: ${device.ident.type.value_raw}.`);
-            return;
-            break;
-          }
-          
-        }
-
         // Generate a unique id for the accessory.
         const uuid = this.api.hap.uuid.generate(deviceObject.uniqueId);
 
@@ -127,38 +109,154 @@ export class MieleAtHomePlatform implements DynamicPlatformPlugin {
         if (existingAccessory) {
           // The accessory already exists
           this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-          // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-          // existingAccessory.context.device = device;
-          // this.api.updatePlatformAccessories([existingAccessory]);
 
-          new platformAccessoryType(this, existingAccessory,
+          const accessoryObj = this.constructPlatformAccessory(device.ident.type.value_raw, existingAccessory,
             deviceObject.modelNumber,
             device.ident.xkmIdentLabel.releaseVersion,
             deviceObject.uniqueId);
+
+          if(!accessoryObj) {
+            this.log.info('Retrieved accessory from cache, but its raw type value is not a supported device. '+
+                          `Device: "${deviceObject.displayName}" `+
+                          `with raw type value: ${device.ident.type.value_raw}.`);
+          }
 
         } else {
           // the accessory does not yet exist, so we need to create it
           this.log.info('Adding new accessory:', deviceObject.displayName);
 
-          // create a new accessory
+          // Create a new accessory
           const accessory = new this.api.platformAccessory(deviceObject.displayName, uuid);
 
-          // store a copy of the device object in the `accessory.context`
+          // Store a copy of the device object in the `accessory.context`
           // the `context` property can be used to store any data about the accessory you may need
           accessory.context.device = deviceObject;
 
-          new platformAccessoryType(this, accessory,
+          const accessoryObj = this.constructPlatformAccessory(device.ident.type.value_raw, accessory,
             deviceObject.modelNumber,
             device.ident.xkmIdentLabel.releaseVersion,
             deviceObject.uniqueId);
 
-          // link the accessory to your platform
-          this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+          if(accessoryObj) {
+            // Link the accessory to your platform
+            this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+          } else {
+            this.log.info('Skipping unsupported device. '+
+                          `Device: "${deviceObject.displayName}" `+
+                          `with raw type value: ${device.ident.type.value_raw}.`);
+          }
         }
-
-
-
       }
-    });
+
+    } catch(response) {
+      if(response.config && response.response) {
+        this.log.error(`Miele API request ${response.config.url} failed with status ${response.response.status}: `+
+                                `"${response.response.statusText}".`);
+      } else {
+        this.log.error(response);
+      }
+    }
+
+
+
+    // request(config, (err, res, body) => {
+    //   if (err) {
+    //     return(this.log.debug(err));
+    //   }
+    //   const response = JSON.parse(body);
+    //   this.log.debug('Platform::discoverDevices: ', response);
+
+    //   const allDevices = Object.keys(response).map(key => response[key]);
+
+    //   // Loop over the discovered devices and register each one if it has not already been registered
+    //   for (const device of allDevices) {
+
+    //     const deviceObject = {
+    //       uniqueId: device.ident.deviceIdentLabel.fabNumber,
+    //       displayName: device.ident.deviceName || device.ident.type.value_localized,
+    //       modelNumber: device.ident.deviceIdentLabel.techType,
+    //     };
+
+    //     this.log.info(`Discovered device: id: ${deviceObject.uniqueId}, `+
+    //                   `name: ${deviceObject.displayName}, model: ${deviceObject.modelNumber}`);
+
+    //     // Generate a unique id for the accessory.
+    //     const uuid = this.api.hap.uuid.generate(deviceObject.uniqueId);
+
+    //     // See if an accessory with the same uuid has already been registered and restored from
+    //     // the cached devices we stored in the `configureAccessory` method above
+    //     const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+
+    //     if (existingAccessory) {
+    //       // The accessory already exists
+    //       this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+
+    //       const accessoryObj = this.constructPlatformAccessory(device.ident.type.value_raw, existingAccessory,
+    //         deviceObject.modelNumber,
+    //         device.ident.xkmIdentLabel.releaseVersion,
+    //         deviceObject.uniqueId);
+
+    //       if(!accessoryObj) {
+    //         this.log.info('Retrieved accessory from cache, but its raw type value is not a supported device. '+
+    //                       `Device: "${deviceObject.displayName}" `+
+    //                       `with raw type value: ${device.ident.type.value_raw}.`);
+    //       }
+
+    //     } else {
+    //       // the accessory does not yet exist, so we need to create it
+    //       this.log.info('Adding new accessory:', deviceObject.displayName);
+
+    //       // Create a new accessory
+    //       const accessory = new this.api.platformAccessory(deviceObject.displayName, uuid);
+
+    //       // Store a copy of the device object in the `accessory.context`
+    //       // the `context` property can be used to store any data about the accessory you may need
+    //       accessory.context.device = deviceObject;
+
+    //       const accessoryObj = this.constructPlatformAccessory(device.ident.type.value_raw, accessory,
+    //         deviceObject.modelNumber,
+    //         device.ident.xkmIdentLabel.releaseVersion,
+    //         deviceObject.uniqueId);
+
+    //       if(accessoryObj) {
+    //         // Link the accessory to your platform
+    //         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+    //       } else {
+    //         this.log.info('Skipping unsupported device. '+
+    //                       `Device: "${deviceObject.displayName}" `+
+    //                       `with raw type value: ${device.ident.type.value_raw}.`);
+    //       }
+    //     }
+
+
+
+    //   }
+    // });
+  }
+
+  //-----------------------------------------------------------------------------------------------
+  // Construct accessory
+  private constructPlatformAccessory(raw_id: number, accessory: PlatformAccessory, 
+    model: string, firmwareRevision: string, serialNumber: string) {
+
+    switch (raw_id) {
+      case this.HOOD_RAW_ID: {
+        // TODO: Change to class deriving from BasePlatformAccessory.
+        return new MieleHoodPlatformAccessory(this, accessory, model, serialNumber);
+        break;
+      }
+
+      case this.WASHER_DRYER_ID:
+      case this.WASHER_ID: {
+        return new MieleWasherDryerPlatformAccessory(this, accessory, model, firmwareRevision, serialNumber);
+        break;
+      }
+      
+      default: {
+        return null;
+        break;
+      }
+      
+    }
   }
 }
